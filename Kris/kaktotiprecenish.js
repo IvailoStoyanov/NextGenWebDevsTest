005252e99200a1d761ca67b0934487c741721b05
Ivaylo Stoyanov <IvailoStoyanov@users.noreply.github.com>
2022-09-01 17:15:06 +0300

    Initial

---
+++ Kris/kaktotiprecenish.js
@@ -0,0 +1,13 @@
+
+// console.log(document.getElementById("#ime"));
+var imeElement = document.getElementById("#ime");
+
+// ime.innerHTML = 'test'
+
+function handleImeCLick() {
+    imeElement.innerHTML = "Rapunzel69"
+}
+
+function handleErase() {
+    imeElement.innerHTML = " "
+}
